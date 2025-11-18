c39c25e2d4f0c500888d0090c36f7776303e20bc
MackSure <bzruzhan@163.com>
2025-11-18 16:12:15 +0000

    finished register router&add userlist page

--- create_db.sql
+++ create_db.sql
@@ -11,6 +11,15 @@ CREATE TABLE IF NOT EXISTS books (
     price  DECIMAL(5, 2),
     PRIMARY KEY(id));
 
+CREATE TABLE IF NOT EXISTS users (
+    id     INT AUTO_INCREMENT,
+    username   VARCHAR(100) NOT NULL,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    hashed_password VARCHAR(256) NOT NULL,
+    PRIMARY KEY(id));
+
 # Create the application user
 CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
 GRANT ALL PRIVILEGES ON berties_books.* TO ' berties_books_app'@'localhost';
--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,50 @@
 const express = require("express")
 const router = express.Router()
 
+const bcrypt = require("bcrypt")
+
+
 router.get('/register', function (req, res, next) {
-    res.render('register.ejs')
+    
+    res.render('register.ejs', {msg: ""})
 })
 
 router.post('/registered', function (req, res, next) {
-    // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
-}); 
+    const saltRound = 10
+    const {first, last, email, password, confirm_password} = req.body;
+    if (!(first&&last&&email&&password)) 
+    return res.render("register.ejs", {msg: "All items required!"})
+    if (password !== confirm_password) 
+    return res.render("register.ejs", {msg: "Confirmed password and the password is not match!"})
+    const query = `SELECT email FROM users WHERE email = (?)`
+    db.query(query, [email], (err, result) => {
+        if (err) return next(err)
+        if (!!result.length) return res.render('register.ejs', {msg: "The email already registered!"})
+    })
+    bcrypt.hash(password, saltRound, (err,hashedPassword) => {
+        if (err) return next(err);
+        const query = `INSERT INTO users (first_name, last_name, email, hashed_password) VALUES(?,?,?,?)`
+        db.query(query, [first, last, email, hashedPassword], (err, result) => {
+            if (err) return next(err);
+            return res.render("index.ejs")
+        })
+    })
+});
+
+// GET /list — Retrieve and display all books from the database
+router.get('/userlist', function(req, res, next) {
+    let sqlquery = "SELECT * FROM users"; // SQL query to get all books
+    
+    // execute sql query
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            // Pass the error to the Express error handler middleware
+            next(err) 
+        }
+        // Renders the list view (list.ejs), passing the query result as 'availableBooks'
+        res.render('userList.ejs', {users:result})
+     });
+});
 
 // Export the router object so index.js can access it
 module.exports = router
